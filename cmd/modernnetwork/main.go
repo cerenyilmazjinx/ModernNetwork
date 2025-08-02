6630bed1a1102cde54968b9a739fa0ccb0073d46
Cereny <56936296+cerenyilmazjinx@users.noreply.github.com>
2025-08-02 19:54:04 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/modernnetwork/main.go
@@ -0,0 +1,23 @@
+// cmd/modernnetwork/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"modernnetwork/internal/modernnetwork"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := modernnetwork.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
